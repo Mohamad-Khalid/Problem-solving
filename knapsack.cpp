ec43d0a33aa8bade356447d1c7b5f337a05a8e15
Mohammed Khalid <61889578+mohamed-khaled1@users.noreply.github.com>
2020-10-08 10:25:32 +0200

    Update knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,31 +1,37 @@
 #include <iostream>
-#include <algorithm>
 #include <cstring>
+
 using namespace std;
+
 int maxW,n, w[1000],p[1000];
-int mem[3000][3000];
+int mem[3000][3000]; // emmoization 
+
 int solve (int i, int remW){
 
-	if(i==n)
+	if(i==n) // base case 
 		return 0;
-	if(mem[i][remW]>-1)
+
+	if(mem[i][remW]>-1) // check if i copmuted before 
 		return mem[i][remW];
 
-	int op1 = solve(i+1, remW);
+	int op1 = solve(i+1, remW);// decision 1 : leave it
 
 	int op2=0;
 	if(remW>=w[i]){
-		op2=solve(i+1, remW-w[i])+ p[i];
+		op2=solve(i+1, remW-w[i])+ p[i]; // decision 2 : pick it
 	}
-	return mem[i][remW]= max(op1,op2);
+	return mem[i][remW]= max(op1,op2); // memoization & return 
 }
 
 int main(){
 	memset(mem,-1,sizeof mem);
+
 	cin>>maxW>>n;
+
 	for(int i=0;i<n;i++)
 		cin>>w[i]>>p[i];
-	cout<<solve(0,maxW);
+
+	cout<<solve(0,maxW);// first state
 
 
 	return 0 ;
