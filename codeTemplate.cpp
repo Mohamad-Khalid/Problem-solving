3f7ea1197c79627baefe9f8d956e003c129426eb
Mohammed Khalid <61889578+mohamed-khaled1@users.noreply.github.com>
2020-11-03 14:07:55 +0200

    Add files via upload

---
+++ codeTemplate.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define	fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define input(n,arr)    for(int i=0;i<n;i++)cin>>arr[i]; // 0 => n
+#define	pi 3.14159265358979323846
+#define	endl "\n"
+#define f first
+#define s second
+
+typedef long long ll;
+typedef unsigned long long ull;
+const ll INF=1e18; 
+const int N = 1e5+5 , oo=0x3f3f3f3f , mod=1e9+7;
+void file(){freopen("out.txt","w",stdout);/*freopen("in.txt","r",stdin);*/}
+
+
+int main(){
+	fast
+	
+	
+	return 0;
+}
