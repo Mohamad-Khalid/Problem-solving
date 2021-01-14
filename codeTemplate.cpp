e0571d33047b4bebfd48cd4ef23c8687ca4bbe0f
Mohammed Khalid <61889578+Mohamad-Khalid@users.noreply.github.com>
2021-01-14 15:44:23 +0200

    Update codeTemplate.cpp

--- codeTemplate.cpp
+++ codeTemplate.cpp
@@ -1,22 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define	fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define input(n,arr)    for(int i=0;i<n;i++)cin>>arr[i]; // 0 => n
-#define	pi 3.14159265358979323846
-#define	endl "\n"
-#define f first
-#define s second
 
+#define	fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define loop(n)  for(int i=0;i<n;++i)
+#define	pi acos(-1) //3.14159265358979323846
+#define	endl '\n'
+	
 typedef long long ll;
 typedef unsigned long long ull;
+
+//         u  d  r   l   ur  dl  ul dr
+int dr[]={-1, 1, 0,  0, -1,  1, -1, 1 };
+int dc[]={ 0, 0, 1, -1,  1, -1, -1, 1 };
+
+
+int compare_double(double a , double b){
+ if(fabs(a-b) < 1e-15 )
+    return 0; // 0 if equlas
+ return a>b? 1 : -1; 
+}
+
 const ll INF=1e18; 
 const int N = 1e5+5 , oo=0x3f3f3f3f , mod=1e9+7;
-void file(){freopen("out.txt","w",stdout);/*freopen("in.txt","r",stdin);*/}
-
 
 int main(){
-	fast
+   fast 
+   int t; cin>>t;
+   while(t--){
+
+
+
+   }
 	
 	
-	return 0;
+return 0;
 }
