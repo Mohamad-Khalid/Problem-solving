30fce0550eb267abac976117ab7f5e69f639819e
Mohammed Khalid <61889578+Mohamad-Khalid@users.noreply.github.com>
2022-02-27 22:12:39 +0200

    Create A.cpp

---
+++ Online Judges/atCoder/ABC241/A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define	fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define loop(n)  for(int i=0;i<n;++i)
+#define	pi acos(-1) //3.14159265358979323846
+#define	endl '\n'
+	
+typedef long long ll;
+typedef unsigned long long ull;
+
+//         u  d  r   l   ur  dl  ul dr
+int dr[]={-1, 1, 0,  0, -1,  1, -1, 1 };
+int dc[]={ 0, 0, 1, -1,  1, -1, -1, 1 };
+
+
+int compare_double(double a , double b){
+ if(fabs(a-b) < 1e-15 )
+    return 0; // 0 if equlas
+ return a>b? 1 : -1; 
+}
+
+const ll INF=1e18; 
+const int N = 1e5+5 , oo=0x3f3f3f3f , mod=1e9+7;
+
+int main(){
+   fast 
+   int n=10;
+   int a[n];
+   for(int i=0;i<n;i++)
+   	cin >> a[i];
+   int f=a[0];
+   int s=a[f];
+   int t=a[s];
+   cout << t << endl;
+	
+	
+return 0;
+}
